dc7159432afd6fbc785720441a65c982115ae309
Flamme2 <flaviemahilyves@gmail.com>
2022-04-05 14:07:36 +0200

    Initial commit

---
+++ src/Animal/Animal.java
@@ -0,0 +1,12 @@
+package Animal;
+
+public class Animal {
+  private String name;
+  private String breed;
+  private double weight;
+
+  Animal(){
+    System.out.println("construction cat:+ name");
+  }
+  public void setName''
+}
---
+++ src/Animal/Cat.java
@@ -0,0 +1,14 @@
+package Animal;
+
+public class Cat extends Animal {
+ // private String name;//
+private String catnip;
+private String typeofCatFood;
+
+
+Cat(String name){
+
+ System.out.println("construction cat : +name");
+}
+
+}
